4f68bea66d800bef17ef70fce460a68a3e7dd39c
aidang1003 <60905689+aidang1003@users.noreply.github.com>
2024-01-30 20:46:39 -0700

    Sepolia
    Completed contract deployment on local network. Now doing the same thing on Sepolia Arb testnet.

---
+++ hardhat.config.js
@@ -0,0 +1,25 @@
+require("@nomicfoundation/hardhat-toolbox");
+
+// NEVER record important private keys in your code - this is for demo purposes
+const SEPOLIA_TESTNET_PRIVATE_KEY =
+  "ac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80";
+const ARBITRUM_MAINNET_TEMPORARY_PRIVATE_KEY = "";
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.18",
+  networks: {
+    hardhat: {
+      chainId: 1337,
+    },
+    arbitrumSepolia: {
+      url: "https://sepolia-rollup.arbitrum.io/rpc",
+      chainId: 421614,
+      accounts: [Sepolia_TESTNET_PRIVATE_KEY],
+    },
+    arbitrumOne: {
+      url: "https://arb1.arbitrum.io/rpc",
+      //accounts: [ARBITRUM_MAINNET_TEMPORARY_PRIVATE_KEY]
+    },
+  },
+};
